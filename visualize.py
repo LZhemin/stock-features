f3a7f69b3b50735af3c602a7ff1339f6725898c6
Liu Zhemin <zhemin_liu@hotmail.com>
2018-05-30 14:02:28 +0800

    visualize using ICA

--- visualize.py
+++ visualize.py
@@ -2,6 +2,7 @@ import os
 import pandas as pd
 from sklearn import preprocessing
 from sklearn.decomposition import PCA
+from sklearn.decomposition import FastICA
 import matplotlib.pyplot as plt
 import argparse
 from config import *
@@ -48,8 +49,42 @@ def visualize_pca():
 
 
 def visualize_ica():
-    print('To be implemented')
-    pass
+    """
+    Function to visualize dataset using ICA to reduce the dimensions
+    Saves the visualization to plots directory
+    Args:
+    Returns:
+    """
+    path = ['data', 'raw', 'stock.csv']
+    data = util.get_data(path)
+    data = data[(data['date'] > START_DATE) & (data['date'] <= END_DATE)].reset_index()
+    del data['date']
+    min_max_scaler = preprocessing.MinMaxScaler(feature_range=(-1, 1))
+    np_scaled = min_max_scaler.fit_transform(data)
+    data = pd.DataFrame(np_scaled)
+    reduced_data = FastICA(n_components=9).fit_transform(data)
+
+    # Plot the decision boundary. For that, we will assign a color to each
+    x_min, x_max = reduced_data[:, 0].min() - 1, reduced_data[:, 0].max() + 1
+    y_min, y_max = reduced_data[:, 1].min() - 1, reduced_data[:, 1].max() + 1
+
+    fig = plt.figure(1)
+    plt.clf()
+
+    plt.xlim(x_min, x_max)
+    plt.ylim(y_min, y_max)
+
+    for index, row in data.iterrows():
+        plt.plot(reduced_data[index, 0], reduced_data[index, 1], '.', markersize=3)
+
+    plt.xticks(())
+    plt.yticks(())
+    plt.title('Visualization of dataset after ICA')
+    plt.show()
+    if not os.path.exists('plots'):
+        os.makedirs('plots')
+    fig.savefig('plots/ICA_Visualization.png')
+    plt.close(fig)
 
 
 def visualize_tsne():
